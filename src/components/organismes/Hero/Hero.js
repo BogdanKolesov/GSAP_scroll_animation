2e67d4a1d27dcba9debb0a192c5707f011f16d19
Bogdan <bogdan_kolesov@bk.ru>
2021-09-16 22:56:40 +0600

    add: first GSAP animation

--- src/components/organismes/Hero/Hero.js
+++ src/components/organismes/Hero/Hero.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { HeroContainer, HeroBackground, HeroWrapper, HeroContent, HeroTitle, HeroText, HeroButton } from './Hero.elements';
-
+import gsap from 'gsap';
 
 const Hero = () => {
+    const heroContent = useRef()
+    useEffect(() => {
+        gsap.fromTo(
+            heroContent.current,
+            { opacity: 0 },
+            { opacity: 1, duration: 2.2 }
+        );
+
+    });
     return (
         <HeroContainer>
             <HeroBackground src={require('../../../assets/images/Hero/Background.jpg').default} />
             <HeroWrapper>
-                <HeroContent>
+                <HeroContent ref={heroContent}>
                     <HeroTitle>Kembang Flower Mantap</HeroTitle>
                     <HeroText>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </HeroText>
                     <HeroButton primary to='/'>Buy it!</HeroButton>
--- src/components/organismes/Relax/Relax.js
+++ src/components/organismes/Relax/Relax.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { RelaxContainer, RelaxContent, RelaxTitle, RelaxText, RelaxContentImage, RelaxImage, RelaxOther } from './Relax.elements';
+import { gsap } from "gsap";
+
 
 const Relax = () => {
     return (
