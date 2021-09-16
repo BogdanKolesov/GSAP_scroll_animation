66f24f18e7ae57c0c49b69e6246c6a975f7bcc83
Bogdan <bogdan_kolesov@bk.ru>
2021-09-16 23:15:04 +0600

    add: Hero GSAP animation

--- src/components/organismes/Hero/Hero.js
+++ src/components/organismes/Hero/Hero.js
@@ -2,21 +2,29 @@ import React, { useEffect, useRef } from 'react';
 import { HeroContainer, HeroBackground, HeroWrapper, HeroContent, HeroTitle, HeroText, HeroButton } from './Hero.elements';
 import gsap from 'gsap';
 
+
 const Hero = () => {
-    const heroContent = useRef()
+    const heroContentRef = useRef()
+    const heroBackgroundRef = useRef()
     useEffect(() => {
         gsap.fromTo(
-            heroContent.current,
+            heroContentRef.current,
             { opacity: 0 },
             { opacity: 1, duration: 2.2 }
+        );
+
+        gsap.fromTo(
+            heroBackgroundRef.current,
+            { opacity: 0 },
+            { opacity: 1, duration: 4 }
         );
 
     });
     return (
         <HeroContainer>
-            <HeroBackground src={require('../../../assets/images/Hero/Background.jpg').default} />
+            <HeroBackground ref={heroBackgroundRef} src={require('../../../assets/images/Hero/Background.jpg').default} />
             <HeroWrapper>
-                <HeroContent ref={heroContent}>
+                <HeroContent ref={heroContentRef}>
                     <HeroTitle>Kembang Flower Mantap</HeroTitle>
                     <HeroText>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </HeroText>
                     <HeroButton primary to='/'>Buy it!</HeroButton>
