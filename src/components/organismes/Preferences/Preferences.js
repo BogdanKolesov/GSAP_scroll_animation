de1209c057f97489e9bcdfb14d6a317a73695934
Bogdan <bogdan_kolesov@bk.ru>
2021-09-17 11:17:50 +0600

    add: GSAP Scroll Trigger

--- src/components/organismes/Preferences/Preferences.js
+++ src/components/organismes/Preferences/Preferences.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { PreferencesData } from '../../../data/PreferencesData';
 import { PreferencesContainer, PreferencesItem, PreferencesTitles, PreferencesIcon, PreferencesTitle, PreferencesText } from './Preferences.elements';
+import { gsap, TimelineLite, Power1 } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Preferences = () => {
+    const t1 = new TimelineLite({ delay: 0.3 });
+    const preferencesRef = useRef()
+
+    useEffect(() => {
+        t1.from(
+            preferencesRef.current,
+            { y: 15, opacity: 0, ease: Power1.easeOut, delay: 0.1 },
+            ScrollTrigger.create({
+                trigger: preferencesRef.current,
+                animation: t1,
+            })
+
+        )
+
+    });
     return (
-        <PreferencesContainer>
+        <PreferencesContainer ref={preferencesRef} >
             {PreferencesData.map((item, index) => (
                 <PreferencesItem key={index}>
                     <PreferencesTitles>
